326d5e13893692e5c9a2292fb464870bd074f8dd
ajiken4610 <ajiken4610@gmail.com>
2024-08-15 14:14:10 +0900

    save

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,11 @@ export default defineConfig({
     lib: {
       // 複数のエントリーポイントのディクショナリや配列にもできます
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es', 'umd'],
+      formats: ['es', 'cjs'],
       name: 'MCVue',
       // 適切な拡張子が追加されます
       fileName: 'mc-vue'
     },
-    minify: 'terser',
     rollupOptions: {
       // ライブラリーにバンドルされるべきではない依存関係を
       // 外部化するようにします
@@ -39,6 +38,6 @@ export default defineConfig({
     }
   },
   define: {
-    __VUE_OPTIONS_API__: 'false'
+    __VUE_OPTIONS_API__: false
   }
 })
